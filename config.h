ee7b433b2a6a5774499ddbc4cc0e9b63bda756fe
Emmy Emmycelium <emm@cybele.dev>
2022-11-13 00:52:10 -0500

    statusbar padding

--- config.h
+++ config.h
@@ -7,6 +7,7 @@ static const unsigned int gappih    = 15;       /* horiz inner gap between windo
 static const unsigned int gappiv    = 15;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 15;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 15;       /* vert outer gap between windows and screen edge */
+static const int vertpadbar         = 2;        /* vertical padding for statusbar */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
