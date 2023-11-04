e444a3abce5b66720393d1e36f3b33be5add1ac0
Emmy <32604310+emm218@users.noreply.github.com>
2023-11-04 19:29:38 -0400

    final state

--- config.h
+++ config.h
@@ -32,7 +32,7 @@ static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" , "10"
 
 static const char *const autostart[] = {
 	"dwmblocks", NULL,
-	"mpdas", NULL,
+	"scritches", NULL,
 	"mpdup", NULL,
 	NULL /* terminate */
 };
